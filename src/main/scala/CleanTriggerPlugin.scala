2a2ffe41bbe713cb433eb106f242fcc13fb557c5
Anatoly Fayngelerin <fanatoly@gmail.com>
2014-04-19 19:41:02 -0400

    passing multi module unit test.

--- src/main/scala/CleanTriggerPlugin.scala
+++ src/main/scala/CleanTriggerPlugin.scala
@@ -8,10 +8,23 @@ object CleanTriggerPlugin extends Plugin{
   val cleanTriggerFile = 
     Def.settingKey[File]("File used to determine whether clean should be run")
 
+  val stampedClean = 
+    Def.taskKey[Unit]("Task to clean and update trigger timestamps")
+
   val triggeredClean =
     Def.taskKey[Unit]("Task that executes the clean task based on whether the trigger is updated")
 
-  private val dynamicClean = Def.taskDyn {
+  private val stampedCleanImpl = Def.task {
+    streams.value.log.info(s"Stamped clean about to delete ${cleanFiles.value} but not ${cleanKeepFiles.value}")
+    clean.value
+    val cacheStamp = Difference.inputs(
+      (cacheDirectory in triggeredClean).value,
+      FilesInfo.lastModified
+    )
+    cacheStamp(Set(cleanTriggerFile.value)){ _ => }
+  }
+
+  private val dynamicClean = Def.taskDyn[Unit] {
     val cacheStamp = Difference.inputs(
       (cacheDirectory in triggeredClean).value,
       FilesInfo.lastModified
@@ -23,9 +36,8 @@ object CleanTriggerPlugin extends Plugin{
 
     if(shouldClean) Def.task{
       streams.value.log.info(s"A change to ${triggerFile} has triggered a clean build")
-      streams.value.log.info(s"A should've deleted ${cleanFiles.value} but not ${cleanKeepFiles}")
-      clean.all(filter).value
-      cacheStamp(Set(cleanTriggerFile.all(filter).value: _*)){ _ => }
+      stampedClean.all(filter).value
+
     } else Def.task {
       streams.value.log.info(s"No changes to ${triggerFile} not running clean")
     }
@@ -38,6 +50,7 @@ object CleanTriggerPlugin extends Plugin{
     triggeredClean := {
       dynamicClean.value
     },
+    stampedClean := { stampedCleanImpl.value },
     (Keys.compile in Compile in ThisProject) <<= (Keys.compile in Compile in ThisProject) dependsOn (triggeredClean)
   )
 }
